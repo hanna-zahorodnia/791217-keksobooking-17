1342c7c2c0955e9886670723009254c3b6da02e2
hanna-zahorodnia <zahorodnia.h@gmail.com>
2019-06-25 21:24:45 +0200

    Добавляет функцию создания объекта xhr

--- js/server.js
+++ js/server.js
@@ -4,24 +4,16 @@
   var URL = 'https://js.dump.academy/keksobooking';
   var dataURL = 'https://js.dump.academy/keksobooking/data';
 
-  var xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
-
-  var upload = function (data, onSuccess, onError) {
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
-      } else {
-        onError('При отправке данных произошла ошибка');
-      }
+  var createXHR = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = 10000; // 10s
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
     });
-
-    xhr.open('POST', URL);
-    xhr.send(data);
-  };
-
-
-  var load = function (onSuccess, onError) {
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
         onSuccess(xhr.response);
@@ -29,15 +21,17 @@
         onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
     });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
+    return xhr;
+  };
 
-    xhr.timeout = 10000; // 10s
+  var upload = function (data, onSuccess, onError) {
+    var xhr = createXHR(onSuccess, onError);
+    xhr.open('POST', URL);
+    xhr.send(data);
+  };
 
+  var load = function (onSuccess, onError) {
+    var xhr = createXHR(onSuccess, onError);
     xhr.open('GET', dataURL);
     xhr.send();
   };
